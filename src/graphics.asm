a9ec3f7fbb3e16ca9ea25e4a266f104982ec5bb0
amirakhaled928 <amirakhaled928@gmail.com>
2024-12-02 22:32:49 +0200

    add draw ball proc

--- src/graphics.asm
+++ src/graphics.asm
@@ -1,9 +1,13 @@
 PUBLIC Draw_Single_Rect
+PUBLIC Draw_Ball
 EXTRN PADDLE_X
 EXTRN PADDLE_Y
 EXTRN PADDLE_WIDTH
 EXTRN PADDLE_HEIGHT
 EXTRN PADDLE_COLOR:BYTE
+EXTRN Ball_X
+EXTRN Ball_Y
+EXTRN Ball_Size
 
 .model small
 .stack 100h
@@ -30,27 +34,55 @@ Draw_Single_Rect proc
     ; mov dx, 10d
     ; mov di, 650d
     ; STORE THE VALUE OF  PADDLE_X IN AX THEN MULTIPLY IT BY 320 AND THEN ADD PADDLE_Y THEN STORE IT IN DI
-                     MOV AX, PADDLE_X
-                     MOV DX, 320
-                     MUL DX
-                     ADD AX, PADDLE_Y
-                     MOV DI, AX
+                         MOV  AX, PADDLE_X
+                         MOV  DX, 320
+                         MUL  DX
+                         ADD  AX, PADDLE_Y
+                         MOV  DI, AX
 
 
-                     MOV DX, PADDLE_HEIGHT
-                     mov si, PADDLE_WIDTH
+                         MOV  DX, PADDLE_HEIGHT
+                         mov  si, PADDLE_WIDTH
     ;  MOV DI, PADDLE_X * 320 + PADDLE_Y
-                     MOV AL, PADDLE_COLOR
-    DRAW_IN_ROW:     
-                     mov bx, di
-                     MOV AL, PADDLE_COLOR
+                         MOV  AL, PADDLE_COLOR
+    DRAW_IN_ROW:         
+                         mov  bx, di
+                         MOV  AL, PADDLE_COLOR
     ;mov al,0eh
     ;mov cx,18
-                     mov cx, si
-                     rep stosb
-                     mov di, bx
-                     add di, 320d
-                     dec dx
-                     jnz DRAW_IN_ROW
+                         mov  cx, si
+                         rep  stosb
+                         mov  di, bx
+                         add  di, 320d
+                         dec  dx
+                         jnz  DRAW_IN_ROW
 Draw_Single_Rect endp
+Draw_Ball PROC
+    ;initialization
+                         MOV  CX,Ball_Y
+                         MOV  DX, Ball_X
+
+    Draw_Ball_Horizontal:
+    ;Update Columns
+                         MOV  AH,0CH
+                         MOV  AL,14
+                         INT  10H
+                         INC  CX
+                         MOV  AX,CX
+                         SUB  AX,Ball_Y
+                         CMP  AX,Ball_Size
+                         JNG  Draw_Ball_Horizontal
+
+    ;Update Rows
+                         MOV  CX,Ball_Y
+                         INC  DX
+                         MOV  AX,DX
+                         SUB  AX,Ball_X
+                         CMP  AX,Ball_Size
+                         JNG  Draw_Ball_Horizontal
+
+                         RET
+                         ENDP Draw_Ball
+
 end Draw_Single_Rect
+
