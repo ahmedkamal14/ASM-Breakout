0ac26fec9fbba5e556f3402e7f54043be20ceaf5
Alyaa Ali <eissaalyaa@gmail.com>
2024-12-18 00:50:03 +0200

    feat: paddle collisions
    and bricks collisions with some issues

--- src/physics.asm
+++ src/physics.asm
@@ -13,6 +13,11 @@ EXTRN PADDLE_Y
 EXTRN PADDLE_X
 EXTRN PADDLE_HEIGHT
 EXTRN PADDLE_WIDTH
+
+EXTRN Bricks_Positions
+EXTRN Brick_Height
+EXTRN Brick_Width
+
 .MODEL SMALL
 .STACK 100h
 
@@ -21,83 +26,148 @@ MOTION PROC
 
 Move_Ball PROC
        
-                   MOV  AX,Ball_Velocity_X
-                   ADD  Ball_X,AX
-                   CMP  Ball_X,0
-                   JLE  Neg_Velocity_X        ;up and down
+                     MOV  AX,Ball_Velocity_X
+                     ADD  Ball_X,AX
+                     CMP  Ball_X,0
+                     JLE  Neg_Velocity_X                 ;up and down
 
-                   MOV  AX,SCREEN_HEIGHT
-                   CMP  Ball_X,AX
-                   JGE  Neg_Velocity_X        ; up and down
-
-
-                   MOV  AX,Ball_Velocity_Y
-                   ADD  Ball_Y,AX
+                     MOV  AX,SCREEN_HEIGHT
+                     CMP  Ball_X,AX
+                     JGE  Neg_Velocity_X                 ; up and down
 
 
-
-                   CMP  Ball_Y,0
-                   JLE  Neg_Velocity_Y
-                   MOV  AX,SCREEN_WIDTH
-                   CMP  Ball_Y,AX
-                   JGE  Neg_Velocity_Y
-
-                   MOV  AX,Ball_X
-                   ADD  AX,Ball_Size
-                   CMP  AX ,PADDLE_X
-                   JNG  STOP
-
-                   MOV  AX,PADDLE_X
-                   ADD  AX,PADDLE_WIDTH
-                   CMP  Ball_X,AX
-                   JNL  STOP
+                     MOV  AX,Ball_Velocity_Y
+                     ADD  Ball_Y,AX
 
 
 
-                   MOV  AX,Ball_Y
-                   ADD  AX,Ball_Size
-                   CMP  AX ,PADDLE_Y
-                   JNG  STOP
+                     CMP  Ball_Y,0
+                     JLE  Neg_Velocity_Y
+                     MOV  AX,SCREEN_WIDTH
+                     CMP  Ball_Y,AX
+                     JGE  Neg_Velocity_Y
 
-                   MOV  AX,PADDLE_Y
-                   ADD  AX,PADDLE_HEIGHT
-                   CMP  Ball_Y,AX
-                   JNL  STOP
+                     CALL Bricks_Collision
+
+                     MOV  AX,Ball_X
+                     ADD  AX,Ball_Size
+                     CMP  AX ,PADDLE_X
+                     JNG  STOP
+
+                     MOV  AX,PADDLE_X
+                     ADD  AX,PADDLE_HEIGHT
+                     CMP  Ball_X,AX
+                     JNL  STOP
 
 
-                   NEG  Ball_Velocity_X
-                   RET
+
+                     MOV  AX,Ball_Y
+                     ADD  AX,Ball_Size
+                     CMP  AX ,PADDLE_Y
+                     JNG  STOP
+
+                     MOV  AX,PADDLE_Y
+                     ADD  AX,PADDLE_WIDTH
+                     CMP  Ball_Y,AX
+                     JNL  STOP
+
+
+                     NEG  Ball_Velocity_X
+                     RET
 
 
 
                    
-    Neg_Velocity_X:
-                   NEG  Ball_Velocity_X
+    Neg_Velocity_X:  
+                     NEG  Ball_Velocity_X
                           
-                   RET
+                     RET
 
-    Neg_Velocity_Y:
-                   NEG  Ball_Velocity_Y
+    Neg_Velocity_Y:  
+                     NEG  Ball_Velocity_Y
                          
-                   RET
+                     RET
 
 
-    STOP:          
-                   RET
+    STOP:            
+                     RET
 Move_Ball ENDP
 Clear_Screen PROC
-                   MOV  AH, 0
-                   MOV  AL, 13H
-                   INT  10H
+                     MOV  AH, 0
+                     MOV  AL, 13H
+                     INT  10H
                 
     ;  MOV  AH,0BH
                         
     ;  MOV  BH,00
     ;  MOV  BL,00
     ;  INT  10H
-                   RET
-                   ENDP Clear_Screen
+                     RET
+                     ENDP Clear_Screen
 
 MOTION ENDP
+Bricks_Collision PROC
+                  
+                     PUSH AX
+                     PUSH BX
+                     PUSH CX
+                     PUSH DX
+                     PUSH SI
+
+                     XOR  SI, SI
+                     MOV  BP, OFFSET Bricks_Positions
+
+    Bricks_Loop:     
+
+                     PUSH SI
+                     SHL  SI,1
+
+    ;Load AX with Brick_X
+                     MOV  AX, [BP + SI]
+                     POP  SI
+                     PUSH SI
+                     INC  SI
+                     SHL  SI,1
+    ;Load Bx with Brick_Y
+                     MOV  BX, [BP + SI]
+                     POP  SI
+                     ADD  SI, 2
+
+                     MOV  CX,Ball_X
+                     ADD  CX,Ball_Size
+                     CMP  CX ,AX
+                     JNG  No_Collision
+
+                     MOV  CX, AX
+                     ADD  CX, Brick_Width
+                     CMP  Ball_X, CX
+                     JNL  No_Collision
+
+
+                     MOV  CX,Ball_Y
+                     ADD  CX,Ball_Size
+                     CMP  CX ,BX
+                     JNG  No_Collision
+
+                     MOV  CX, BX
+                     ADD  CX, Brick_Height
+                     CMP  Ball_Y,CX
+                     JNL  No_Collision
+
+
+                     NEG  Ball_Velocity_X    
+
+    No_Collision:            
+                     CMP  SI, 64
+                     JL   Bricks_Loop
+
+                     POP  SI
+                     POP  DX
+                     POP  CX
+                     POP  BX
+                     POP  AX
+
+                     RET
+                     ENDP Bricks_Collision
 
  END MOTION
